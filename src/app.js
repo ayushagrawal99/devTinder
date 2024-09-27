1516abbd7ae0bc555c3c82a9565b7a2a6e5f89eb
Ayush <ayushagrawal9470@gmail.com>
2024-09-27 17:58:47 +0530

    added model and validations

--- src/app.js
+++ src/app.js
@@ -2,6 +2,94 @@ const express = require('express');
 const PORT = 3000;
 const app = express();
 const dbConnect = require('./config/database');
+const User = require('./models/user');
+
+// parse the JSON obj which comes from client.
+app.use(express.json());
+
+app.post('/signup', async (req,res,next) => {
+    const userObj = req.body;
+
+    const user = new User(userObj);
+
+    try {
+        await user.save();
+        res.send('User save successfully') 
+    } catch (error) {
+        res.status(400).send("Error saving the user data "+error)
+    }
+})
+
+// Get a user
+app.get('/user', async (req,res,next) => {
+    try {
+        let email = req.body.emailId;
+
+        let user = await User.findOne({emailId: email});
+
+        if(user){
+            res.send(user) 
+        } else {
+            res.status(404).send("user not found")
+        }
+    } catch (error) {
+        res.status(400).send("Error fetching a the user data")
+    }
+})
+
+// Get All user
+app.get('/feed', async (req,res,next) => {
+    try {
+        let user = await User.find({});
+
+        if(user){
+            res.send(user) 
+        } else {
+            res.status(404).send("user not found")
+        } 
+    } catch (error) {
+        res.status(400).send("Error fetching the user data")
+    }
+})
+
+// Delete a user
+app.delete('/user', async (req,res,next) => {
+    try {
+        let userId = req.body.userId;
+        let user = await User.findByIdAndDelete(userId);
+        
+        res.send("user deleted successfully")
+        
+    } catch (error) {
+        res.status(400).send("Error in user delete")
+    }
+})
+
+// Update a user
+app.patch('/user/:userId', async (req,res,next) => {
+    try {
+        let userId = req?.params.userId;
+        let data = req.body;
+
+        const ALLOWED_INPUTS = ["age", "gender", "photoUrl", "about", "skills"];
+
+        const isAllowed = Object.keys(data).every((el) => ALLOWED_INPUTS.includes(el));
+        if(!isAllowed){
+            throw new Error("Some data are not allowed to update")
+        }
+
+        if(data?.skills.length > 10){
+            throw new Error("Can update max 10 skills")
+        }
+
+        let user = await User.findByIdAndUpdate(userId, data, {new: true, runValidators: true});
+ 
+        res.send("user updated successfully");
+    } catch (error) {
+        res.status(400).send("VALIDATION ERROR " + error)
+    }
+})
+
 
 dbConnect()
     .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,66 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName: {
         type : String,
+        require: true,
+        maxLength: 20,
+        trim: true
     },
     lastName: {
-        type : String
+        type : String,
+        maxLength: 20,
+        trim: true
     },
     emailId: {
-        type : String
+        type : String,
+        require: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate(value) {
+            if(!validator.isEmail(value)){
+                throw Error("Email ID format is Wrong "+ value)
+            }
+        }
     },
     password: {
-        type : String
+        type : String,
+        require: true,
+        trim: true,
+        validate(value) {
+            if(!validator.isStrongPassword(value)){
+                throw Error("Password is not strong "+ value)
+            }
+        }
     },
     age: {
-        type : Number
+        type : Number,
+        min: 18
     },
     gender: {
-        type : String
-    }
+        type : String,
+        validate(value){
+            if(!["male", "female", "others"].includes(value)){
+                throw Error("this gender is not allowed ", value)
+            }
+        }
+    },
+    photoUrl: {
+        type : String,
+        trim: true
+    },
+    about: {
+        type : String,
+        default: "This is default value",
+        trim: true
+    },
+    skills: {
+        type : [String]
+    },
+}, {
+    timestamps: true
 });
 
 const User = mongoose.model('User', userSchema);
