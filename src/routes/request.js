5971c68ec93b739eb9ef08510d41f30cc8b43405
Ayush <ayushagrawal9470@gmail.com>
2024-09-28 18:02:10 +0530

    creatd connection accept API

--- src/routes/request.js
+++ src/routes/request.js
@@ -5,7 +5,7 @@ const {userAuth} = require('../middleware/auth');
 
 const requestRouter = express.Router();
 
-requestRouter.post('/request/send/:status/:toUserId', userAuth, async (req,res,next) => {
+requestRouter.post('/request/send/:status/:toUserId', userAuth, async (req,res) => {
     try {
         const fromUserId = req.user._id;
         const toUserId = req.params.toUserId;
@@ -47,6 +47,42 @@ requestRouter.post('/request/send/:status/:toUserId', userAuth, async (req,res,n
 
         res.json({
             message: `${req.user.firstName} is send ${status} to ${userCheck.firstName}`
+        })
+    } catch (error) {
+        res.status(400).send("Error "+error)
+    }
+})
+
+requestRouter.post('/request/review/:status/:requestId', userAuth, async (req,res) => {
+    try {
+        const loggedInUser = req.user;
+        const requestId = req.params.requestId;
+        const status = req.params.status;
+
+        let ALLOWED_STATUS = ["accepted", "rejected"];
+        if(!ALLOWED_STATUS.includes(status)){
+            return res.status(400).json({
+                message: `${status} Status not allowed`
+            })
+        }
+
+        let connectionRequestDt = await connectionRequest.findOne({
+            _id: requestId,
+            toUserId: loggedInUser._id,
+            status: "intrested"
+        })
+        if(!connectionRequestDt){
+            return res.status(400).json({
+                message: "Connection request not found"
+            })
+        }
+
+        connectionRequestDt.status = status;
+
+        await connectionRequestDt.save();
+
+        res.json({
+            message: `Connection stablished successfully`
         })
     } catch (error) {
         res.status(400).send("Error "+error)
